16a27224f3914038e1b9f9b51f5039cf4eb1d85c
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2026-09-10 12:25:10 +0200

    Add package doc; update namespace

---
+++ R/famnesia-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+#' @importFrom DT datatable
+#' @importFrom bslib page_sidebar
+#' @importFrom htmltools HTML
+#' @importFrom pedFamilias readFam
+#' @importFrom pedmut getParams
+#' @importFrom pedprobr likelihood
+#' @importFrom pedtools typedMembers
+#' @importFrom shinyjs useShinyjs
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
